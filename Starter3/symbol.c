1231390d70e36d59a24ca6a33a6dac1517a9fd3c
huchenlei <chenlei.hu@mail.utoronto.ca>
2017-11-15 15:53:18 -0500

    add dummy implementation of symbol table

--- Starter3/symbol.c
+++ Starter3/symbol.c
@@ -5,10 +5,20 @@
 #include "symbol.h"
 
 void scope_enter() {
-
+    printf("symbol: Entering scope\n");
 }
 
-void scope_leave();
-size_t scope_depth();
-int scope_declare_symbol(st_entry st);
-st_entry* scope_find_entry(const char* id);
+void scope_leave() {
+    printf("symbol: Leaving scope\n");
+}
+size_t scope_depth() {
+    return 0;
+}
+
+int scope_declare_symbol(st_entry st) {
+    return 0;
+}
+
+st_entry* scope_find_entry(const char* id) {
+    return (st_entry*)NULL;
+}
